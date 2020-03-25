2cab1b68681d2ef5774636857eab13a091c7047a
Leo <l.yoon17@gmail.com>
2020-03-24 23:31:20 -0600

    Fixed scenario:
    	- When dropdown menu is deployed, and screen is enlarged, the
    	dropdown would remain. Now the dropdown disappears when the
    	screen is enlarged.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,36 @@
 function menuBarOnClick(bars) {
     bars.classList.toggle("change");
+
+    // Show dropdown menu onClick
+    document.getElementById("myDropdown").classList.toggle("show");
+
+}
+
+// Remove dropdown menu onclick on anywhere else on the screen
+
+window.onclick = function(event) {
+
+  this.console.log(event.target.className);
+
+  if (!event.target.matches(".menu-container")) {
+
+    var bars = document.getElementsByClassName("menu-container");
+    var barsId = document.getElementById("myMenuContainer");
+
+    if (barsId.className == "menu-container change") {
+      for (var i = 0; i < bars.length; i++) {
+        bars[i].classList.toggle("change");
+      }
+    }
+    
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+
 }
